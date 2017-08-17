cb0394d261202c88ecaebba22bcaf0565f530475
Andre Rahardjo <zertozzf@gmail.com>
2017-08-18 05:45:37 +0700

    Maintain code consistency
    Remove useless line

--- 404.php
+++ 404.php
@@ -11,9 +11,7 @@
             <div class="error-content">
                 <br>
                 <h3><i class="fa fa-warning text-yellow"></i> Oops! Page not found.</h3>
-                <p>
-                    We could not find the page you were looking for.<br> Or maybe you are not allowed to access this page!
-                </p>
+                <p>We could not find the page you were looking for.<br> Or maybe you are not allowed to access this page!</p>
             </div>
             <!-- /.error-content -->
         </div>
